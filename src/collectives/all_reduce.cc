3e18d704715a7214aad786189bf2609c71f7c063
Le Xu <sharonxu65@gmail.com>
2020-10-16 18:27:32 -0500

    printing CommArgs

--- src/collectives/all_reduce.cc
+++ src/collectives/all_reduce.cc
@@ -12,7 +12,8 @@ NCCL_API(ncclResult_t, ncclAllReduce, const void* sendbuff, void* recvbuff, size
     ncclDataType_t datatype, ncclRedOp_t op, ncclComm* comm, cudaStream_t stream);
 ncclResult_t ncclAllReduce(const void* sendbuff, void* recvbuff, size_t count,
     ncclDataType_t datatype, ncclRedOp_t op, ncclComm* comm, cudaStream_t stream) {
-	INFO(NCCL_COLL, "create dummy tagger: nccl: AllReduce");
+	INFO(NCCL_COLL, "create dummy tagger: nccl: AllReduce rank %d, nRanks %d, node %d, nNodes %d, localRanks %d, opCount %d, lastOpCount %d, nChannels %d, p2pnChannels %d, p2pnChannelsPerPeer %d "
+     comm->rank, comm->nRanks, comm->node, comm->nNodes, comm->localRanks, comm->opCount, comm->lastOpCount, comm->nChannels, comm->p2pnChannels, comm->p2pnChannelsPerPeer );
 	nvtxTagger dummyTagger = nvtxTagger("nccl: AllReduce");
   struct ncclInfo info = { ncclCollAllReduce, "AllReduce",
     sendbuff, recvbuff, count, datatype, op, 0, comm, stream, /* Args */
